c2b7abca38d004b2a60ba24d7af478b48e7dbd0b
RadioMan116 <noris75564@gmail.com>
2019-07-23 11:44:20 +0300

    1231

--- assets/js/min.js
+++ assets/js/min.js
@@ -10691,34 +10691,7 @@
 	return jQuery;
 });
 $(document).ready(function () {
-	var qAll = document.querySelectorAll('div'),
-		dId = document.getElementById('main'),
-		TagName = document.getElementsByTagName('h1'),
-		ClassName = document.getElementsByClassName('main');
 
-	const cros = {
-		name: 'Nike',
-		prise: 1000,
-		count: 3
-	};
-
-	// arr.forEach(function (element) {
-	// 	console.log(element);
-	// });
-	// arr.forEach(function (propName, index) {
-	// 	console.log(arr[index]);
-	// 	console.log(cros[propName]);
-	// });
-	Object.keys(cros).forEach(function (key) {
-		console.log(key, cros[key]);
-	});
-
-	// console.log(cros);
-
-	// console.log(qAll);
-	// console.log(dId);
-	// console.log(TagName);
-	// console.log(ClassName);
 	var swiper = new Swiper('.right .swiper-container', {
 		slidesPerView: 1,
 		spaceBetween: 0,
